516cccfbe58f95451b220e83880ea658dce9bdb9
Greg Allen <mail@jga23.com>
2010-11-19 07:35:54 -0800

    updated appui

--- www/ui/scripts/app.lawnchair.js
+++ www/ui/scripts/app.lawnchair.js
@@ -133,6 +133,10 @@ Array.prototype.extend = function(array) {
 		this.push(array[i]);
 	}
 }
+
+Array.prototype.last = function() {
+	return this[this.length - 1];
+}
 if (typeof debug === "undefined") {
 	window.debug = console;
 }
@@ -345,7 +349,7 @@ var Application = Controller.extend({
 		var self = this;
 
         window.addEventListener("load", function() { 
-            if (browser.isMobile) {
+            if (false && browser.isMobile) {
                 document.addEventListener("deviceready", function() { self.ready() }, false);
             } else {
                 self.ready() 
