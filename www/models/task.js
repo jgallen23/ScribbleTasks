6d3970e11c328bc6167444c949d0321a3c362c64
Greg Allen <mail@jga23.com>
2010-11-25 09:41:13 -0800

    changed dates to timestamp, fixed sorting

--- www/models/task.js
+++ www/models/task.js
@@ -11,19 +11,31 @@ var Task = Model.extend({
 			modifiedOn: null
 		}
 		this._super(initial);
-		this.createdOn = new Date();
+		if (!this.createdOn)
+			this.createdOn = new Date().getTime();
+
+
+		if (typeof this._data.createdOn === "string")
+			this._data.createdOn = new Date(this._data.createdOn).getTime();
+
+		if (typeof this._data.modifiedOn === "string")
+			this._data.modifiedOn = new Date(this._data.modifiedOn).getTime();
+
+		if (this.completedOn && typeof this._data.completedOn === "string")
+			this._data.completedOn = new Date(this._data.completedOn).getTime();
+
 		this.__defineGetter__("isComplete", this._isComplete);
 		this.__defineProperty__("star", this._getStar, this._setStar);
 	},
 	_propertySet: function(prop, value) {
-		this._data.modifiedOn = new Date();
+		this._data.modifiedOn = new Date().getTime();
 		this._super(prop, value);
 	},
 	save: function(cb) {
 		Task.data.save(this, cb);
 	},
 	complete: function() {
-		this.completedOn = new Date();
+		this.completedOn = new Date().getTime();
 	},
 	unComplete: function() {
 		this.completedOn = null;
@@ -59,5 +71,17 @@ Task.sort = {
 			return b.modifiedOn - a.modifiedOn;
 		}
 		return star;
+	},
+	incomplete: function(a, b) {
+		var s1 = (a.star)?1:0;
+		var s2 = (b.star)?1:0;
+		var star = s2 - s1;
+		if (star == 0) {
+			return b.modifiedOn - a.modifiedOn;
+		}
+		return star;
+	},
+	complete: function(a, b) {
+		return b.completedOn - a.completedOn;
 	}
 }
