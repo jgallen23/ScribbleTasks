4dc29b925f1996042734ffaf87352b71d7c996ec
Greg Allen <mail@jga23.com>
2010-12-03 08:04:05 -0800

    moved index js into app.js

--- www/ui/scripts/ScribbleTasksApp.js
+++ www/ui/scripts/ScribbleTasksApp.js
@@ -1,4 +1,67 @@
 var ScribbleTasksApp = Application.extend({
+	ready: function() {
+		var self = this;
+		this._super();
+		if (this.browser.isMobile) {
+			self.disableScrollingPermanently();
+		}
+		this.resize();
+		this.currentController = new ProjectListController("ProjectList");
+		this.runTests();
+	},
+	resize: function(width, height) {
+		this._super(width, height);
+		if (this.browser.isPhoneGap)
+			height -= 20;
+		console.log(height);
+		if (height < 500) {
+			document.querySelector("#AddTask .wrapper").style.height = height+"px";
+		}
+		var wrapper = document.querySelector("#Wrapper");
+		height -= (wrapper.offsetTop*2);	
+		var containers = document.querySelectorAll(".Container");
+		for (var i = 0, c = containers.length; i < c; i++) {
+			containers[i].style.height = height+"px";
+		}
+	},
+	runTests: function() {
+		var self = this;
+		var params = parseQueryString();
+		//* Tests
+		if (params.test) {
+			if (params.test == "1" || params.test == "2" || params.test == "4") {
+				var f = false;
+				this.currentController.bind("loaded", function() {
+					if (f) return;
+					var pc = this.showProject(this.projects[0]);
+					if (params.test == "2")
+						pc.showAddTask();
+					if (params.test == "4")
+						pc.onClick.menu();
+					f = true;
+
+				});
+			} else if (params.test == "3") {
+				this.currentController.onClick.debug();
+			} else if (params.test == "search") {
+				this.currentController.bind("loaded", function() {
+					this.onClick.search.call(this);
+					//c.showSearch("Starred Tasks", Task.filters.star);
+				});
+			}
+		}
+		//*/
+		return;
+		this.backup(function(data) { 
+			var textarea = document.createElement("textarea");
+			textarea.style.height = "400px";
+			textarea.style.width = "100%";
+
+			textarea.value = data;
+			document.body.innerHTML = ""; 
+			document.body.appendChild(textarea);
+		});
+	},
     updateBadge: function() {
 		console.log("Badge: "+this.data.badgeCount);
         if (this.browser.isPhoneGap) {
